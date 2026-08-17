349272de460625a2ebb1b6902823b8ac58fcf2ae
SHKim <bll3479@gmail.com>
2026-08-17 19:47:29 +0900

    +0817: update train_origin.py

--- train_original.py
+++ train_original.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torchvision import models
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
-from model import bulid_model
+from Models.model import bulid_model
 
 
 #transform 정의
@@ -30,4 +32,51 @@ images, labels = next(iter(train_loader))
 #print(images.shape, labels.shape)
 
 model = bulid_model(num_classes=15)
-print(model)
+#print(model)
+
+#손실함수, 최적화 설정
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr = 0.001)
+
+#학습 루프 
+num_epochs= 1
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+
+    for images, labels in tqdm(train_loader):
+        #1. 기울기 초기화
+        optimizer.zero_grad()
+        #2. 순전파
+        outputs = model(images)
+        #3. 손실 계산
+        loss = criterion(outputs, labels)
+        #4. 역전파
+        loss.backward()
+        #5. 가중치 업데이트
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print(f'Epoch {epoch+1}, Loss : {running_loss/len(train_loader):.4f}')
+
+#검증 루프
+val_dir = r'./Data/Validation/01_Source_Data'
+val_dataset = datasets.ImageFolder(root=val_dir, transform = transform)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+print(len(val_dataset))
+
+model.eval()
+correct, total = 0, 0
+
+with torch.no_grad():
+    for images, labels in tqdm(val_loader):
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+accuracy = correct / total
+print(f'Validation Accuracy: {accuracy*100:.2f}%')
